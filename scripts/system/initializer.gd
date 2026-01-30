0064d0adb6217e45d0a329b0552e9f92271f0244
Fábio A M Santos <116984019+fbsagat@users.noreply.github.com>
2026-01-30 17:56:24 -0300

    Câmera melhorada + start server bat corrigido

--- scripts/system/initializer.gd
+++ scripts/system/initializer.gd
@@ -3,7 +3,7 @@ extends Node
 # Configurações
 ## [TESTES] Usa o TestManager para iniciar logo uma partida na execução
 ## (configura server e clients / clients recebem localhost_auto_connect = true)
-@export var test_mode: bool = false
+@export var test_mode: bool = true
 ## [TESTES] Define a quantidade de instâcias de clientes conectadas para executar fast_round
 @export var simulador_players_qtd: int = 2
 ## [TESTES] Dropa itens perto dos players e ativa o trainer de cada player
