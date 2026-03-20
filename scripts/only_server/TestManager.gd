7b0a2e39108e80fee811e0c19f56110733e1c982
Fábio A M Santos <116984019+fbsagat@users.noreply.github.com>
2026-03-20 09:38:59 -0300

    Correção, test manager agora enviar rpc de nomes de clientes
    _client_name_accepted executado em test manager agora

--- scripts/only_server/TestManager.gd
+++ scripts/only_server/TestManager.gd
@@ -113,6 +113,9 @@ func criar_partida_teste(nome_sala: String = "Sala de Teste", configuracoes_roun
 		#var player_uuid = client_registry.get_uuid_by_peer_id(peer_id)
 		var player_name = "TestPlayer%d" % [i + 1]
 		var success = client_registry.register_player_name(_uuid_base, player_name)
+		
+		# Atualiza o cliente
+		network_manager.rpc_id(peer_id, "_client_name_accepted", player_name)
 
 		if not success:
 			_log_debug("❌ Falha ao registrar jogador %d" % _uuid_base)
